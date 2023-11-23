4d0d156d94be56193882acd67350250f9b9a7914
Nguyễn Ngọc Đức <duco060303@gmail.com>
2023-11-23 16:01:17 +0700

    Update menu page, create detail food

--- Project_KFC_WEB/Controllers/HomeController.cs
+++ Project_KFC_WEB/Controllers/HomeController.cs
@@ -14,15 +14,27 @@ namespace Project_KFC_WEB.Controllers
         public ActionResult Index() 
         {
             ViewBag.foodCategories = db.foodCategories.ToList();
+            ViewBag.Length = db.foodCategories.ToList().Count();
 
             return View(db.foods.ToList());
         }
 
-        public ActionResult Menu()
+        public ActionResult Menu(int index = 0)
         {
             ViewBag.foodCategories = db.foodCategories.ToList();
+            ViewBag.Length = db.foodCategories.ToList().Count();
+            ViewBag.currentIndex = index;
 
             return View(db.foods.ToList());
+        }
+
+        public ActionResult FoodDetail(int id = 1)
+        {
+            var idCategory = db.foods.ToList().FirstOrDefault(item => item.id == id).idCategory;
+            ViewBag.category = db.foodCategories.ToList().FirstOrDefault(item => item.id == idCategory);
+            ViewBag.index = db.foodCategories.ToList().FindIndex(item => item.id == idCategory);
+
+            return View(db.foods.ToList().FirstOrDefault( item => item.id == id));
         }
     }
 }
